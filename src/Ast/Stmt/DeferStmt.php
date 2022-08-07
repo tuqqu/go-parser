140720e6e83f296ee4ffb866cd7d740205ade639
tuqqu <artkurbidaev@gmail.com>
2022-08-07 03:22:40 +0300

    DeferStmt expr type changed to CallExpr

--- src/Ast/Stmt/DeferStmt.php
+++ src/Ast/Stmt/DeferStmt.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace GoParser\Ast\Stmt;
 
-use GoParser\Ast\Expr\Expr;
+use GoParser\Ast\Expr\CallExpr;
 use GoParser\Ast\Keyword;
 
 final class DeferStmt implements Stmt
 {
     public function __construct(
         public readonly Keyword $keyword,
-        public readonly Expr $expr,
+        public readonly CallExpr $expr,
     ) {}
 }
